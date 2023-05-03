9efc59428e2e7188e7e8eb264afaa7c92b83b4bc
sug48 <mpsugarman@gmail.com>
2023-05-03 01:53:25 -0400

    adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 
