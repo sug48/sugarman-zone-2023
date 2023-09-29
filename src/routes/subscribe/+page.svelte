3d1bee21e11c0bf711b4e44045d6dc771b7a36f2
sug48 <mpsugarman@gmail.com>
2023-09-29 11:13:39 -0700

    update

--- src/routes/subscribe/+page.svelte
+++ src/routes/subscribe/+page.svelte
@@ -1,6 +1,6 @@
 
 <p>
-    Subscribe for really infrequent updates.ç
+    Subscribe for really infrequent updates.
     <br />
     <br />
     <iframe src="https://embeds.beehiiv.com/56b8fe70-32ce-491c-a980-c1b17143d817?slim=true" data-test-id="beehiiv-embed" height="52" frameborder="0" scrolling="no" style="margin: 0; border-radius: 0px !important; background-color: transparent;" title="mailing list sign-up"></iframe>
